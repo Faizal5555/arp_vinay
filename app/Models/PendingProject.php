e8c38584b8f089c51a02503dfec0a5f7733f2b10
DivakarTechzar <faizal.techzar@gmail.com>
2025-06-03 16:14:54 +0530

    client name changes

--- app/Models/PendingProject.php
+++ app/Models/PendingProject.php
@@ -39,7 +39,7 @@ class PendingProject extends Model
 
     public function client()
     {
-        return $this->belongsTo(Client::class);
+       return $this->belongsTo(Client::class, 'client_id', 'id');
     }
     
 }
